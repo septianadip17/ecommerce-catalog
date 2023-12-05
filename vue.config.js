f1e3dc84d8c8443644fbcd3b1f38fe144cc968d4
Septian Adi Pratama <septianadip7998@gmail.com>
2023-12-06 00:02:01 +0700

    merge css

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = 'Task_5';
+      return args;
+    });
+  },
+};
